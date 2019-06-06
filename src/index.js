458e1d69044fecb1ab1e14c8719d611d8d87f887
Jeff Yamada <jeff@impossible-bureau.com>
2019-06-06 17:50:54 -0600

    first commit with all tests passing

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {*} zip should be a string or number.
+ * String is preferred since APO zips can start with zero
+ */
+const isZipAPO = (zip) => {
+  const zipString = zip.toString();
+  const firstThree = zipString.slice(0, 3);
+  const apoPrefixes = ['340', '090', '091', '092', '093', '094', '095', '096', '097', '098', '962', '963', '964', '965', '966'];
+  const isApo = apoPrefixes.indexOf(firstThree) !== -1;
+  return isApo;
+};
+
+const isStateAPO = (code) => {
+  const apoCodes = ['AE', 'AA', 'AP'];
+  const isApo = apoCodes.indexOf(code) !== -1;
+  return isApo;
+};
+
+export default isZipAPO;
+
+export { isStateAPO };
